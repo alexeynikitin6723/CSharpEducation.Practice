ca1c408cfd13336557ce2cbab51376b299fb2310
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-15 22:23:27 +0400

    Add Practice1.Task4

---
+++ Practice1/Practice1.Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice1.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую фразу:");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Введите вторую фразу:");
+            string phrase2 = Console.ReadLine();
+            Console.WriteLine($"{phrase} {phrase2}");
+        }
+    }
+}
