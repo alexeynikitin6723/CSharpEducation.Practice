7633a3f17c084c40db5447ee30358c6972b7668e
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-15 22:20:57 +0400

    Add Practice1.Task3

---
+++ Practice1/Practice1.Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice1.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string myName = Console.ReadLine();
+            Console.WriteLine($"Hello {myName}");
+        }
+    }
+}
