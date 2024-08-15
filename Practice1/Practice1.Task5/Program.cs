4bca5ef8b168ff1ccfa88d251cc781f37930dc73
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-15 22:26:41 +0400

    Add Practice1.Task5

---
+++ Practice1/Practice1.Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice1.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            double r = 3.14;
+            Console.WriteLine($"Площадь круга радиуса {r} равна {r*PI}");
+        }
+    }
+}
