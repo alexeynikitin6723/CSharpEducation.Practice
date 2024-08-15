3e33a63f4325e92b1f4be117b8c0840beadc89c3
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-15 22:18:33 +0400

    add Practice1.Task2

---
+++ Practice1/Practice1.Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice1.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            string name = "Alexey";
+            float temperature = 12.5f;
+            bool sexIsWoman = false;
+        }
+    }
+}
