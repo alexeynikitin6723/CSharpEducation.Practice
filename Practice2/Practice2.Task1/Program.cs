5303c573d641b7d96d493f3d569ee6b925b13df8
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:29:29 +0400

    Add Practice2.Task1

---
+++ Practice2/Practice2.Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Practice2.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int j = 0;
+            while (j < 25)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            }while(k < 25);
+
+            List<string> phrase = new List<string>();
+            Console.WriteLine("Для выхода из цикла нажмите 'q'");
+            for (; ; )
+            {
+                string word = Console.ReadLine();
+                if (word.ToLower() == "q")
+                {
+                    break;
+                }
+                phrase.Add(word);
+            }
+            Console.WriteLine(String.Join(" ", phrase.ToArray()));
+
+            List<string> phrase2 = new List<string>();
+            Console.WriteLine("Для выхода из цикла нажмите 'q'");
+            while(true)
+            {
+                string word = Console.ReadLine();
+                if (word.ToLower() == "q")
+                {
+                    break;
+                }
+                phrase2.Add(word);
+            }
+            Console.WriteLine(String.Join(" ", phrase2.ToArray()));
+
+            List<string> phrase3 = new List<string>();
+            Console.WriteLine("Для выхода из цикла нажмите 'q'");
+            do 
+            {
+                string word = Console.ReadLine();
+                if (word.ToLower() == "q")
+                {
+                    break;
+                }
+                phrase3.Add(word);
+            }while(true);
+            Console.WriteLine(String.Join(" ", phrase3.ToArray()));
+        }
+    }
+}
