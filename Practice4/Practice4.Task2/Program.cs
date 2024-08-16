d7571b2b5fad220bc27006e2a6bfd51f0ae19e88
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:47:55 +0400

    Add Practice4.Task2

---
+++ Practice4/Practice4.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Practice4.Task1;
+
+namespace Practice4.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.54;
+            double y = 2.51;
+            int x1 = 4;
+            int y1 = 5;
+            Console.WriteLine(MathHelper.Add(x,y));
+            Console.WriteLine(MathHelper.Substract(x,y));
+            Console.WriteLine(MathHelper.Multiple(x,y));
+            Console.WriteLine(MathHelper.Divide(x,y));
+
+            Console.WriteLine(MathHelper.Add(x1, y1));
+            Console.WriteLine(MathHelper.Substract(x1, y1));
+            Console.WriteLine(MathHelper.Multiple(x1, y1));
+            Console.WriteLine(MathHelper.Divide(x1, y1));
+        }
+    }
+}
