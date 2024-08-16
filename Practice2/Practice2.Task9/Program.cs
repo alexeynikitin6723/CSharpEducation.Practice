c3319f2db4cf0929d08bf4397eacf2fb77cdbbb7
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:07 +0400

    Add Practice2.Task9

---
+++ Practice2/Practice2.Task9/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice2.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintArr(NewArr(5));
+        }
+
+        static int[] NewArr(int size)
+        {
+            int[] arr = new int[size];
+            return arr;
+        }
+
+        static void PrintArr(int[] arr)
+        {
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
