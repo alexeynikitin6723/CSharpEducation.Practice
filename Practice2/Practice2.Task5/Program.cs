877f3843c0c7a2ef03ad7ffaf45605eef5206ab7
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:06 +0400

    Add Practice2.Task5

---
+++ Practice2/Practice2.Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice2.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] xy = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int x = xy[0];
+            int y = xy[1];
+            Console.WriteLine(CompareInt(x, y));
+        }
+
+        static bool CompareInt(int x, int y)
+        {
+            return x == y;
+        }
+    }
+}
