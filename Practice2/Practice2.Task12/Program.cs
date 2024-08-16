63f7acb05d05738b283067813412ff54b147b64b
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:08 +0400

    Add Practice2.Task12

---
+++ Practice2/Practice2.Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice2.Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
