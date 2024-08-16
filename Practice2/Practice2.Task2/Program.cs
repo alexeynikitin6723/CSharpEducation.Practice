ccfd0256a959f680137f07581b548690ec6c5247
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:29:39 +0400

    Add Practice2.Task2

---
+++ Practice2/Practice2.Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practice2.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DegreeCToDegreeF(100));
+        }
+
+        static double MeterToKm(double length)
+        {
+            return length/1000;
+        }
+
+        static double CmToKm(double length)
+        {
+            return length*1000*100;
+        }
+
+        static double MeterPerSecToKmPerHour(double speed)
+        {
+            return speed * 3.6;
+        }
+
+        static double DegreeCToDegreeF(double temperature)
+        {
+            return 32 + temperature * 1.8;
+        }
+    }
+}
