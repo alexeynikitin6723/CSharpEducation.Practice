24aa903d9d4df66fdf1473668007699be4c124ca
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:09 +0400

    Add Practice2.Task15

---
+++ Practice2/Practice2.Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice2.Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Исходный массив:");
+            PrintArray(arr);
+
+            int[] inverteArray = InvertArray(arr);
+
+            Console.WriteLine("\nИнвертированный массив:");
+            PrintArray(inverteArray);
+        }
+        static int[] InvertArray(int[] array)
+        {
+            Array.Reverse(array);
+            return array;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int e in array)
+            {
+                Console.Write(e + " ");
+            }
+        }
+    }
+}
