348e1655a2bbb8ccee940fb83ef6e9ea2ec92765
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:47:55 +0400

    Add Practice4.Task1

---
+++ Practice4/Practice4.Task1/MathHelper.cs
@@ -0,0 +1,78 @@
+﻿namespace Practice4.Task1
+{
+    public class MathHelper
+    {
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiple(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Деление на 0");
+            }
+            return x / y;
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Substract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiple(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Деление на 0");
+            }
+            return x / y;
+        }
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Substract(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Multiple(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Divide(float x, float y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Деление на 0");
+            }
+            return x / y;
+        }
+    }
+}
