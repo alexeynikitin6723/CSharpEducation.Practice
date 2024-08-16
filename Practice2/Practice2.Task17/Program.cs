b844254dbca3e107be5181f4df39ca0523cf5ba3
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:10 +0400

    Add Practice2.Task17

---
+++ Practice2/Practice2.Task17/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice2.Task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Swap(ref x, ref y);
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+
+        static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
