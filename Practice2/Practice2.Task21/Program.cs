46384d9e0eb06831d5329e8aec5d07466ad05123
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:56 +0400

    Add Practice2.Task21

---
+++ Practice2/Practice2.Task21/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n = r.Next(1, 100);
+            Console.WriteLine("Введите число.\nДля выхода нажмите 0");
+            while(true)
+            {
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 0)
+                {
+                    break;
+                }
+                if (answer == n) {
+                    Console.WriteLine("Вы угадали");
+                    break;
+                }
+            }
+        }
+    }
+}
