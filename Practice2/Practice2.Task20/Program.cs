ff17c80858ab177207ca009dedd50099afd24771
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:55 +0400

    Add Practice2.Task20

---
+++ Practice2/Practice2.Task20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace Practice2.Task20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n = r.Next(50, 100);
+            int m = r.Next(50, 100);
+            int[,] arr = new int[n,m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = r.Next(1, 100);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int max = 0;
+                int min = 100;
+                for (int j = 0; j < m; j++)
+                {
+                    if(max < arr[i, j])
+                    {
+                        max = arr[i, j];
+                    }
+                    if (min > arr[i, j])
+                    {
+                        min = arr[i, j];
+                    }
+                }
+                Console.WriteLine($"Строка {i}: {max-min}");
+            }
+
+        }
+    }
+}
