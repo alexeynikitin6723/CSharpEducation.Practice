f9635c958e9b971549affd0c2b3f260a1e70465c
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:58 +0400

    Add Practice3.Task7

---
+++ Practice3/Practice3.Task7/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Practice3.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthEnum month = MonthEnum.December;
+            Console.WriteLine(NumberOfDaysInMonth(month));
+        }
+
+        public enum MonthEnum
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        public static int NumberOfDaysInMonth(MonthEnum month)
+        {
+            switch (month)
+            {
+                case MonthEnum.January:
+                case MonthEnum.March:
+                case MonthEnum.May:
+                case MonthEnum.July:
+                case MonthEnum.August:
+                case MonthEnum.October:
+                case MonthEnum.December:
+                    return 31;
+                case MonthEnum.April:
+                case MonthEnum.June:
+                case MonthEnum.September:
+                case MonthEnum.November:
+                    return 30;
+                case MonthEnum.February:
+                    return 28;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
