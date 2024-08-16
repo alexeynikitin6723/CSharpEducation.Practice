29c793f5802f7a23004907b24473c81ba18410b6
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:59 +0400

    Add Practice3.Task8

---
+++ Practice3/Practice3.Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5,6);
+            Console.WriteLine(rect.Square());
+        }
+    }
+}
---
+++ Practice3/Practice3.Task8/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Practice3.Task8
+{
+    public struct Rectangle
+    {
+        public double height;
+        public double width;
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public double Square()
+        {
+            return height * width;
+        }
+    }
+}
