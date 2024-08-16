5402f49b23c64a34361a0e1eca0de2ca0126cd11
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:09 +0400

    Add Practice2.Task13

---
+++ Practice2/Practice2.Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice2.Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArr(NewArr(5,6));
+        }
+
+        static int[,] NewArr(int m, int n)
+        {
+            int[,] arr = new int[m,n];
+            Random random = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i,j] = random.Next(0, 100);
+                }
+            }    
+            return arr;
+        }
+
+        static void PrintArr(int[,] arr)
+        {
+            int rows = arr.GetUpperBound(0)+1;
+            int columns = arr.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{arr[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
