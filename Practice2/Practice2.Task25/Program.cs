9027331ecdc1146c9345506e233a43e307504fed
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:57 +0400

    Add Practice2.Task25

---
+++ Practice2/Practice2.Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice2.Task25
+{
+    internal class Program
+    {
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 7:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            DayOfWeek day = (DayOfWeek)input;
+            Console.WriteLine($"Выбранный день недели: {day}");
+        }
+    }
+}
