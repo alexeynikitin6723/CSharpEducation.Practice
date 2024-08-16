8695b9da86ce04ac5c0eb59767f169c4429b532b
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:57 +0400

    Add Practice3.Task3

---
+++ Practice3/Practice3.Task3/Calculator.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Practice3.Task3
+{
+    public class Calculator
+    {
+        public Calculator() { }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на 0");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Practice3.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 2;
+            Console.WriteLine($"Add: {Calculator.Add(a,b)}");
+            Console.WriteLine($"Substract: {Calculator.Substract(a,b)}");
+            Console.WriteLine($"Multiply: {Calculator.Multiply(a,b)}");
+            Console.WriteLine($"Divide: {Calculator.Divide(a,b)}");
+
+        }
+    }
+}
