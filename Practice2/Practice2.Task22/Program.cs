cde4428d61ee4725a475583e2761a1e67adec68b
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:56 +0400

    Add Practice2.Task22

---
+++ Practice2/Practice2.Task22/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Practice2.Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n = r.Next(1, 100);
+            int attempt = 0;
+            Console.WriteLine("Правила:\nДля выхода нажмите 0\nКоличество попыток 10\nВведите число:");
+            while (true)
+            {
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 0)
+                {
+                    break;
+                }
+                if (answer == n)
+                {
+                    Console.WriteLine("Вы угадали");
+                    break;
+                }
+                if (answer < n)
+                {
+                    Console.WriteLine($"Загаданное число больше {answer}");
+                }
+                else
+                {
+                    Console.WriteLine($"Загаданное число меньше {answer}");
+                }
+                attempt++;
+                if (attempt == 10)
+                {
+                    Console.WriteLine($"Вы проиграли");
+                }
+            }
+        }
+    }
+}
