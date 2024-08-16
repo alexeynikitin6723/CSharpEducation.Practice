2de915f5c02e34543c5b5286d439b7dc4baec947
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:08 +0400

    Add Practice2.Task11

---
+++ Practice2/Practice2.Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice2.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[int.Parse(args[0])];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(args[1]);
+            }
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
