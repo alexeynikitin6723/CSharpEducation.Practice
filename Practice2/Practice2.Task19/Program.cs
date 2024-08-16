9269f79d89343c6bbbe36d60df4c2f78940e531a
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:10 +0400

    Add Practice2.Task19

---
+++ Practice2/Practice2.Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Practice2.Task19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int len = r.Next(50, 100);
+            int[] arr = new int[len];
+            for(int i = 0; i < len; i++)
+            {
+                arr[i] = r.Next(1, 100);
+            }
+            int min = arr.OrderBy(x => x).First();
+            int max = arr.OrderBy(x => x).Last();
+            Console.WriteLine($"{max - min}");
+        }
+    }
+}
