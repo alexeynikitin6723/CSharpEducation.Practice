21933cd1984b2cd2020914fb0b6e4a3bec40dbba
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:58 +0400

    Add Practice3.Task6

---
+++ Practice3/Practice3.Task6/Point.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Practice3.Task6
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+        
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static double Length(Point p1, Point p2)
+        {
+            return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
+        }
+    }
+}
---
+++ Practice3/Practice3.Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3,4);
+            Point p2 = new Point(6,9);
+            Console.WriteLine(Point.Length(p1, p2));
+        }
+    }
+}
