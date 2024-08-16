582f36077b0a2579a4123566abcc3e22d03355cb
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:57 +0400

    Add Practice2.Task24

---
+++ Practice2/Practice2.Task24/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice2.Task24
+{
+    internal class Program
+    {
+        enum DayOfWeek
+        {
+            Monday,
+            Tuesday, 
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дни недели:");
+            foreach(DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine(day.ToString());
+            }
+        }
+    }
+}
