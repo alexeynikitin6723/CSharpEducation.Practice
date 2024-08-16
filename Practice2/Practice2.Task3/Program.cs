954ad9e8cc3495d61b42f4dd487296b28332f2d9
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:29:53 +0400

    Add Practice2.Task3

---
+++ Practice2/Practice2.Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice2.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvaluateExpression(0,4,5));
+        }
+
+        static double EvaluateExpression(double a, double b, double f)
+        {
+            if (a == 0) {
+                throw new DivideByZeroException("Деление на 0");
+            }
+            return (a + b - f / a) + f * a * a - (a + b);
+        }
+    }
+}
