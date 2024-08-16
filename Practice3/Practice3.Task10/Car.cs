69055623b09fb5b6f6829198ba5caabb97b7b797
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:59 +0400

    Add Practice3.Task10

---
+++ Practice3/Practice3.Task10/Car.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Practice3.Task10
+{
+    public class Car
+    {
+        private string brandCar;
+
+        public string BrandCar
+        {
+            get => brandCar;
+            set => brandCar = value;
+        }
+        public static void PrintBrandCar(Car car)
+        {
+            Console.WriteLine($"Brand: {car.brandCar}");
+        }
+    }
+}
---
+++ Practice3/Practice3.Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car() { BrandCar = "tesla" };
+            Car.PrintBrandCar(car);
+        }
+    }
+}
