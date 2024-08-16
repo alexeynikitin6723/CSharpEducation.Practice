e37e6717cfd79f0a912ae268637db2faeda7c613
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:09 +0400

    Add Practice2.Task14

---
+++ Practice2/Practice2.Task14/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice2.Tasks14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = NewArr(5);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+            Console.WriteLine(String.Join(" ", arr));
+        }
+
+        static int[] NewArr(int n)
+        {
+            return new int[n];
+        }
+    }
+}
