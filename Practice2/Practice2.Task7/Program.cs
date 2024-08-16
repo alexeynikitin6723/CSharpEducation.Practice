e83a682c623533c0b256b4eb55793c92b4ce7259
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:07 +0400

    Add Practice2.Task7

---
+++ Practice2/Practice2.Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            Console.WriteLine(str.ToLower());
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(char.ToUpper(str[0])+str.Substring(1).ToLower());
+        }
+
+    }
+}
