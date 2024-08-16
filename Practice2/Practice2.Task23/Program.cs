7b70d81ab0b2772872bdc21006fddbcb20df9fd8
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:41:56 +0400

    Add Practice2.Task23

---
+++ Practice2/Practice2.Task23/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice2.Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите год:");
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine("Год не високосный");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный");
+            }
+        }
+    }
+}
