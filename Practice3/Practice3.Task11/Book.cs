3c47592b6ffca0bfde11523c6b4e3b669541c604
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:46:00 +0400

    Add Practice3.Task11

---
+++ Practice3/Practice3.Task11/Book.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Practice3.Task11
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+
+
+        public string Title
+        {
+            get => this.title;
+            set => this.title = value;
+        }
+        public string Author
+        {
+            get => this.author;
+            set => this.author = value;
+        }
+
+        public static string GetBookInformation(Book book)
+        {
+            return $"Title: {book.Title}, Author: {book.Author}";
+        }
+    }
+}
---
+++ Practice3/Practice3.Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book() { Author = "Ford", Title = "StoryTale" };
+            Console.WriteLine(Book.GetBookInformation(book));
+        }
+    }
+}
