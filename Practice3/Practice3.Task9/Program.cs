a27c6efc52deef60f28a9cd4f66c9f3d7cee1fd7
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:59 +0400

    Add Practice3.Task9

---
+++ Practice3/Practice3.Task9/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Fix", 0);
+            Student.ChangeName(student);
+            Console.WriteLine(student.Name);
+        }
+    }
+}
---
+++ Practice3/Practice3.Task9/Student.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Practice3.Task9
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+        public Student() : this("Unnamed", 0) { }
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name {
+            get => this.name;
+            set => this.name = value;
+        }
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public static void ChangeName(Student student)
+        {
+            student.Name = "Аноним";
+        }
+    }
+}
