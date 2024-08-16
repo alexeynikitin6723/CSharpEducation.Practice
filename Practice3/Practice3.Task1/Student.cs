bbe6afddb43038fd326d5d82b32ef5fc52e98fb2
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:56 +0400

    Add Practice3.Task1

---
+++ Practice3/Practice3.Task1/Student.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Practice3.Task1
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+        public Student() : this("Unnamed", 0) { }
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name {
+            get => this.name;
+            set => this.name = value;
+        }
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {this.Name}, возраст: {this.Age}");
+        }
+    }
+}
