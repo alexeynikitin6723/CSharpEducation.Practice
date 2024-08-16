5b65cc114306809c9b049e89f9e48a8c5ff61b76
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:08 +0400

    Add Practice2.Task10

---
+++ Practice2/Practice2.Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Practice2.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[int.Parse(args[0])];
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
