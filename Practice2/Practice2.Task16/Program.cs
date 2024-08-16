e7571e2624d8793d7866bc7b3e123b99e371b9fa
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:09 +0400

    Add Practice2.Task16

---
+++ Practice2/Practice2.Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            OppositeSign(ref n);
+            Console.WriteLine(n);
+        }
+
+        static void OppositeSign(ref int n)
+        {
+            n = -n; 
+        }
+    }
+}
