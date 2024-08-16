ed5b14c76de3bf0b92ad38d5f5e08933c5f32ff7
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:47:55 +0400

    Add Practice4.Task3

---
+++ Practice4/Practice4.Task3/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice4.Task3
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+        public Person(): this("NoName", 0) { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void PrintInformation()
+        {
+            Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+        }
+    }
+}
