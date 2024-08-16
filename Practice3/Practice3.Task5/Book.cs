52797ad79b5895b7d5f601b3ff5150ae09df373b
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:58 +0400

    Add Practice3.Task5

---
+++ Practice3/Practice3.Task5/Book.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Practice3.Task5
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int yearOfPublishing;
+
+        /*public Book()
+        {
+            this.title = "Unnamed";
+            this.author = "Unnamed";
+        }*/
+        public Book(string title) : this(title, "unnamed", 0) { }
+        public Book(string title, string author) : this(title, author, 0) { }
+        public Book(string title, string author, int year)
+        {
+            this.title = title;
+            this.author = author;
+            this.yearOfPublishing = year;
+        }
+
+        public string Title
+        {
+            get => this.title;
+        }
+        public string Author
+        {
+            get => this.author;
+            set => this.author = value;
+        }
+        public int YearOfPublishing
+        {
+            get => this.yearOfPublishing;
+            set => this.yearOfPublishing = value;
+        }
+    }
+}
---
+++ Practice3/Practice3.Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice3.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Chemisty");
+            Console.WriteLine(book.Author);
+        }
+    }
+}
