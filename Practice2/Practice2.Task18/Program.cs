b5fb3712eac376e5a5d7a640749324bd19fe6b5c
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:10 +0400

    Add Practice2.Task18

---
+++ Practice2/Practice2.Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Practice2.Task18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, -2, 4, -6, 8, 10, 11, 12 };
+            NonNegaticeNumber(arr, out int count);
+            Console.WriteLine($"Arr: {String.Join(" ", arr)}, Neg: {count}");
+        }
+
+        static int[] NonNegaticeNumber(int[] array, out int delete)
+        {
+            delete = array.Where(x => x < 0).Count();
+            return array.Where(x => x >= 0).ToArray();
+        }
+    }
+}
