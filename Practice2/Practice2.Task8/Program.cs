0b17054f6a349d2c64d81408564606bbd68db640
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:33:07 +0400

    Add Practice2.Task8

---
+++ Practice2/Practice2.Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = abc[0];
+            int b = abc[1];
+            int c = abc[2];
+            if (a == b || a == c || b == c)
+            {
+                a += 5;
+                b += 5;
+                c += 5;
+            }
+            else
+            {
+                Console.WriteLine("равных нет");
+            }
+        }
+
+    }
+}
