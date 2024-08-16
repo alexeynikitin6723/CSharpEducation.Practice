8bfa9c2303e4228c5dcdf06e28fcd37405d38b21
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:45:57 +0400

    Add Practice3.Task4

---
+++ Practice3/Practice3.Task4/Book.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Practice3.Task4
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+
+        public Book()
+        {
+            this.title = "Unnamed";
+            this.author = "Unnamed";
+        }
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public string Title
+        {
+            get => this.title;
+        }
+        public string Author
+        {
+            get => this.author;
+        }
+    }
+}
