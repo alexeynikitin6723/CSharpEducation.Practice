687e3ac0fe2bec7c6e454ae3925dffb8d2826ded
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-16 21:47:56 +0400

    Add Practice4.Task3-2

---
+++ Practice4/Practice4.Task3-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice4.Task3;
+
+namespace Practice4.Task3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person("Марина", 25);
+            Person p3 = new Person("Владимир", 53);
+            p1.PrintInformation();
+            p2.PrintInformation();
+            p3.PrintInformation();
+        }
+    }
+}
